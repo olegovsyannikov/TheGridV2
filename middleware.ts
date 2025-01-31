a8b2c5119809e3ca557a56dd2ec5157884c41bf0
olegovsyannikov <oleg.ovsyannikov@gmail.com>
2025-01-31 11:08:57 +0400

    Fixed public folder access

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public (public files)
+     * - public files
      */
-    '/((?!_next/static|_next/image|favicon.ico|public).*)'
+    '/((?!_next/static|_next/image|favicon.ico)(?!.*\\.).*)',
+    '/(api|trpc)(.*)'
   ]
 };
